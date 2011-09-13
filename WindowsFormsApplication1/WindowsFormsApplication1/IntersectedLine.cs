21e43c571d6b2de1d32fa0d8b611fc07a647254f
Paul French <pauladrianfrench@hotmail.com>
2011-09-13 22:24:32 +0100

    New files added to project

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/IntersectedLine.cs
@@ -0,0 +1,12 @@
+﻿namespace Rooms
+{
+    using System.Drawing;
+    public class IntersectedLine : Line
+    {
+        public IntersectedLine(Point p1, Point p2)
+            : base(p1, p2)
+        {
+
+        }
+    }
+}
